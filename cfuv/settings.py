bd462412dd0a416c33a0cb2bc6b1dc1f8d3711c5
mb1te <45900239+mb1te@users.noreply.github.com>
2020-08-07 18:47:44 +0300

    del django tables

--- cfuv/settings.py
+++ cfuv/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ratings',
-    'django_tables2'
+    'ratings'
 ]
 
 MIDDLEWARE = [
